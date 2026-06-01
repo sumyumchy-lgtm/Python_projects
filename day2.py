2d8a6acdd3ddf832466ba9c8e38d7b9df2eee70f
sumyumchy-lgtm <sumyumchy@gmail.com>
2026-06-01 07:15:57 +0545

    Delete day2.py

--- day2.py
+++
@@ -1,6 +0,0 @@
-num1=int(input("Enter first number:"))
-num2=int(input("Enter second number:"))
-print("First number is",num1,"and second number is",num2)
-print("Sum is",num1+num2)
-print("Multiplication is",num1*num2)
-print("Substraction is", num1-num2)
