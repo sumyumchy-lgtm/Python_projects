224d22f12be8a0afe6fcc1d1fb7c4ef8737598cd
sumyumchy-lgtm <sumyumchy@gmail.com>
2026-06-01 07:15:02 +0545

    Add comparison script for two input numbers

---
+++ Basics/day4.py
@@ -0,0 +1,10 @@
+a,b=map(int,input("Enter two numbers:").split())
+
+if a < b :
+    print(a,"is greater than",b)
+
+elif a > b :
+    print(b,"is greater than",a)
+
+else :
+    print("They are equal.")
