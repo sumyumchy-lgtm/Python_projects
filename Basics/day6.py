be9eb438d1c0180bec67361390bc22812fff464d
sumyumchy-lgtm <sumyumchy@gmail.com>
2026-06-01 13:52:48 +0545

    Add files via upload

---
+++ Basics/day6.py
@@ -0,0 +1,10 @@
+usrnme="admin"
+psswd="admin123"
+username=input("Enter username:")
+password=input("Enter password:")
+
+if username==usrnme and password==psswd:
+    print("Login successful!")
+
+else:
+    print("Invalid username or password. Please try again.")
